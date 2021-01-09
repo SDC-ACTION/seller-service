7517bafd438fcb2eb8fa35c041555cc07035e3de
anabellchan <anbellchan@gmail.com>
2021-01-08 17:27:15 -0800

    Use ENV on MySQL connection properties

--- database/mysql/index.js
+++ database/mysql/index.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql');
+require('dotenv').config();
 
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   database: 'shopping',
 });
 
