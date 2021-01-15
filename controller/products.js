4c00800d8356c80fd08489484aec06bef3250fb2
anabellchan <anbellchan@gmail.com>
2021-01-15 14:37:36 -0800

    Add Redis client

--- controller/products.js
+++ controller/products.js
@@ -1,8 +1,21 @@
 /* eslint-disable consistent-return */
 /* eslint-disable no-restricted-globals */
+const redis = require('redis');
 const { retrievePrices, retrieveAllPrices } = require('../database/mysql/prices');
 const { retrieveSellers } = require('../database/mysql/sellers');
 const { createQuotes } = require('../services/mysql/quotes');
+
+const client = redis.createClient({
+  port: 6379,
+  host: '34.217.79.104',
+});
+
+client.on('error', (error) => {
+  console.error(error);
+});
+client.on('ready', () => {
+  console.log('connected to Redis');
+});
 
 const prices = (req, res) => {
   if (req.params.productId !== undefined) {
@@ -41,19 +54,44 @@ const quotes = (req, res) => {
     return res.status(400).send('Bad Request.');
   }
 
-  retrieveSellers(id)
-    .then((data) => createQuotes(data, id))
-    .then((data) => {
-      if (data.length > 0) {
-        res.send(data);
-      } else {
-        res.status(404).send('product not found');
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-      res.sendStatus(500);
-    });
+  const returnCachedValue = (data) => {
+    console.log('*** cache hit');
+    const value = JSON.parse(data);
+    if (value.length > 0) {
+      res.send(value);
+    } else {
+      res.status(404).send('product not found');
+    }
+  };
+
+  const returnDatabaseValue = () => {
+    console.log('--- cache miss');
+    retrieveSellers(id)
+      .then((data) => createQuotes(data, id))
+      .then((data) => {
+        if (data.length > 0) {
+          client.set(id, JSON.stringify(data), (error, reply) => {
+            console.log('+++ set key result: ', reply);
+          });
+          res.send(data);
+        } else {
+          res.status(404).send('product not found');
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+      });
+  };
+
+  // check the cache server for the data first
+  client.get(id, (err, value) => {
+    if (value) {
+      returnCachedValue(value);
+    } else {
+      returnDatabaseValue();
+    }
+  });
 };
 
 module.exports = {
